5925194d0e7d6d975a25af085ab854d6af7205a5
JoeVincentt <eugene.bb@hotmail.com>
2019-01-13 19:49:40 -0500

    signup finished, added mutation and error handling as well as form validation

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import { SIGNUP_USER } from "../../queries/index";
+import Error from "../Error";
+
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  passwordConfirmation: ""
+};
 
 class Signup extends Component {
-  state = {
-    username: "",
-    email: "",
-    password: "",
-    passwordConfirmation: ""
-  };
+  state = { ...initialState };
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
+  };
+
+  clearState = () => {
+    this.setState({ ...initialState });
+  };
+
+  handleSubmit = (event, signupUser) => {
+    event.preventDefault();
+    signupUser().then(data => {
+      console.log(data);
+      this.clearState();
+    });
+  };
+
+  validateForm = () => {
+    const { username, password, email, passwordConfirmation } = this.state;
+    const isInvalid =
+      !username || !password || !email || password !== passwordConfirmation;
+    return isInvalid;
   };
 
   render() {
@@ -17,39 +41,56 @@ class Signup extends Component {
     return (
       <div className="App">
         <h2 className="App">Signup</h2>
-        <form className="form">
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            onChange={this.handleChange}
-            value={username}
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            onChange={this.handleChange}
-            value={email}
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={this.handleChange}
-            value={password}
-          />
-          <input
-            type="password"
-            name="passwordConfirmation"
-            placeholder="Confirm Password"
-            onChange={this.handleChange}
-            value={passwordConfirmation}
-          />
-          <button type="submit" className="button-primary">
-            Submit
-          </button>
-        </form>
+        <Mutation
+          mutation={SIGNUP_USER}
+          variables={{ username, email, password }}
+        >
+          {(signupUser, { data, loading, error }) => {
+            return (
+              <form
+                className="form"
+                onSubmit={event => this.handleSubmit(event, signupUser)}
+              >
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  onChange={this.handleChange}
+                  value={username}
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  onChange={this.handleChange}
+                  value={email}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                  value={password}
+                />
+                <input
+                  type="password"
+                  name="passwordConfirmation"
+                  placeholder="Confirm Password"
+                  onChange={this.handleChange}
+                  value={passwordConfirmation}
+                />
+                <button
+                  type="submit"
+                  className="button-primary"
+                  disabled={loading || this.validateForm()}
+                >
+                  Submit
+                </button>
+                {error && <Error error={error} />}
+              </form>
+            );
+          }}
+        </Mutation>
       </div>
     );
   }
